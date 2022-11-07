9b2a034dba3b141a34500529cf6aa4b53077f2bd
kashishdipro <kamanashishdatta@gmail.com>
2022-11-07 20:24:25 +0600

    added jwt token for secure multiple routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -9,10 +10,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kdtr5cm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT (req, res, next){
+    // console.log(req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'Unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'Forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run(){
     try{
         const serviceCollection = client.db("geniusCar").collection("services");
@@ -33,8 +49,14 @@ async function run(){
         })
 
         //Orders
-        app.get('/orders', async(req, res) =>{
+        app.get('/orders', verifyJWT, async(req, res) =>{
+            // console.log(req.headers.authorization);
             let query = {};
+            const decoded = req.decoded;
+            if(decoded.email !== req.query.email){
+                return res.status(403).send({message: 'Forbidden access'})
+            }
+            console.log(decoded);
             if(req.query.email){
                 query = {
                     email: req.query.email
@@ -45,20 +67,20 @@ async function run(){
             res.send(orders);
         })
 
-        app.post('/orders', async(req, res) =>{
+        app.post('/orders', verifyJWT, async(req, res) =>{
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
         })
 
-        app.delete('/orders/:id', async(req, res) =>{
+        app.delete('/orders/:id', verifyJWT, async(req, res) =>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const result = await orderCollection.deleteOne(query);
             res.send(result);
         })
 
-        app.patch('/orders/:id', async(req, res) =>{
+        app.patch('/orders/:id', verifyJWT, async(req, res) =>{
             const id = req.params.id;
             const status = req.body.status
             const query = {_id: ObjectId(id)}
@@ -69,6 +91,14 @@ async function run(){
             }
             const result = await orderCollection.updateOne(query, UpdatedDoc);
             res.send(result);
+        })
+
+        //JWT
+        app.post('/jwt', (req, res) =>{
+            const user = req.body;
+            // console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '100'})
+            res.send({token})
         })
 
     }finally{
